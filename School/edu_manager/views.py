503ec59e945e8949e8fb423637074122e0801d50
tester <fenrirwinner@gmail.com>
2025-04-24 11:05:07 +0500

    views permission

--- School/edu_manager/views.py
+++ School/edu_manager/views.py
@@ -8,23 +8,29 @@ from .serializers import (ClassSerializer, StudentSerializer,
                           TeacherWithPrimaryKeySerializer, AdminSerializers,
                           StudentFullSerializer, TeacherFullSerializer)
 
+from .permissions import IsAdmin, IsStudent, IsTeacher
+
 # Create your views here.
 
 class AdminViewSet(ModelViewSet):
     queryset = Admin.objects.all()
     serializer_class = AdminSerializers
+    permission_classes = [IsAdmin]
 
 
 class TeacherViewSet(ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherFullSerializer
+    permission_classes = [IsTeacher | IsAdmin]
 
 
 class ClassViewSet(ModelViewSet):
     queryset = Class.objects.all()
     serializer_class = ClassWithStudentsSerializer
+    permission_classes = [IsAdmin | IsTeacher | IsStudent]
 
 
 class StudentViewSet(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentFullSerializer
+    permission_classes = [IsAdmin | IsTeacher | IsStudent]
